3c740d891b49162c939f68eb671f34eaab585f69
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-30 20:18:20 +0530

    remote database

--- video tutorials/java/conn.inc.php
+++ video tutorials/java/conn.inc.php
@@ -1,11 +1,13 @@
 <?php 
 	
 	   
-	   $con=mysqli_connect('localhost','root');
+	 /*  $con=mysqli_connect('localhost','root');  */
+ $con=mysqli_connect('remotemysql.com','n8gxap7PGT','hIeFLjY7Qe');
+			
 	   if (!$con) {
 	   	die('connection failed'.mysqli_connect_error());
 	   }
 
-	mysqli_select_db($con,'uniquedeveloper');
-
+	/* mysqli_select_db($con,'uniquedeveloper'); */
+mysqli_select_db($con,'n8gxap7PGT');
  ?>
