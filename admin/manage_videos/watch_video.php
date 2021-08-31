2aebb17f30a31fb169939ecc4263891912add3e8
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-31 10:02:32 +0530

    Update watch_video.php

--- admin/manage_videos/watch_video.php
+++ admin/manage_videos/watch_video.php
@@ -1,7 +1,8 @@
 <?php
-   $con=mysqli_connect('localhost','root');
+  $con=mysqli_connect('remotemysql.com','n8gxap7PGT','hIeFLjY7Qe');
 
-  mysqli_select_db($con,'uniquedeveloper');
+
+			mysqli_select_db($con,'n8gxap7PGT');
     require 'comments.inc.php';      //including comment code
 ?>
 <!DOCTYPE html>
