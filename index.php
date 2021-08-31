67dcc0ef13af986187ec2df0eaa8669a40dd7b6f
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-31 11:27:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
 
-
-error_reporting(1);
-
-
-
-
 session_start();
-
-
-if (!isset($_SESSION['username'])) {
-	header('location:login.php');
+if(!isset($_SESSION['username'])) {
+	include('login.php');
 	# code...
 
 }
