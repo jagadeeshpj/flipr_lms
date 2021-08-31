6d7c245aa862f9f1cb7b5b1e6d1fdc6168357bbe
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-31 11:30:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 session_start();
 if(!isset($_SESSION['username'])) {
-	include('login.php');
+	header('location:login.php');
 	# code...
 
 }
@@ -328,12 +328,12 @@ if(!isset($_SESSION['username'])) {
 
 								<?php 
 
-								  $con=mysqli_connect('localhost','root');
+								  $con=mysqli_connect('remotemysql.com','n8gxap7PGT','hIeFLjY7Qe');
 									   if (!$con) {
 									   	die('connection failed'.mysqli_connect_error());
 									   }
 
-									mysqli_select_db($con,'uniquedeveloper');
+									mysqli_select_db($con,'n8gxap7PGT');
 
 									$sql="select * from faq";
 									$result=mysqli_query($con,$sql);
