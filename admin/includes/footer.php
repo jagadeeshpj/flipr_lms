a42547e4c35987637ef3070aee52bb7d6b448daf
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-01 20:27:12 +0530

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -3,230 +3,327 @@
 <head>
     <title></title>
     <style type="text/css">
- .site-footer
+
+
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
 {
-  background-color:#26272b;
-  padding:45px 0 20px;
-  font-size:15px;
-  line-height:24px;
-  color:#737373;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins" , sans-serif;
 }
-.site-footer hr
-{
-  border-top-color:#bbb;
-  opacity:0.5
+body{
+  min-height: 100vh;
+  width: 100%;
+  background: #EEECEB;
 }
-.site-footer hr.small
-{
-  margin:20px 0
+footer{
+  position: relative;
+  background: #000;
+  width: 100%;
+  bottom: 0;
+  left: 0;
 }
-.site-footer h6
-{
-  color:#fff;
-  font-size:16px;
-  text-transform:uppercase;
-  margin-top:5px;
-  letter-spacing:2px
+footer::before{
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 100px;
+  height: 1px;
+  width: 100%;
+  background: #000;
 }
-.site-footer a
-{
-  color:#737373;
+footer .content{
+  max-width: 1250px;
+  margin: auto;
+  padding: 30px 40px 40px 40px;
 }
-.site-footer a:hover
-{
-  color:#3366cc;
-  text-decoration:none;
+footer .content .top{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 50px;
 }
-.footer-links
-{
-  padding-left:0;
-  list-style:none
+.content .top .logo-details{
+  color: #fff;
+  font-size: 30px;
 }
-.footer-links li
-{
-  display:block
+.content .top .media-icons{
+  display: flex;
 }
-.footer-links a
-{
-  color:#737373
+.content .top .media-icons a{
+  height: 40px;
+  width: 40px;
+  margin: 0 8px;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 40px;
+  color: #fff;
+  font-size: 17px;
+  text-decoration: none;
+  transition: all 0.4s ease;
 }
-.footer-links a:active,.footer-links a:focus,.footer-links a:hover
-{
-  color:#3366cc;
-  text-decoration:none;
+.top .media-icons a:nth-child(1){
+  background: #4267B2;
 }
-.footer-links.inline li
-{
-  display:inline-block
+.top .media-icons a:nth-child(1):hover{
+  color: #4267B2;
+  background: #fff;
 }
-.site-footer .social-icons
-{
-  text-align:right
+.top .media-icons a:nth-child(2){
+  background: #E1306C;
 }
-.site-footer .social-icons a
-{
-  width:40px;
-  height:40px;
-  line-height:40px;
-  margin-left:6px;
-  margin-right:0;
-  border-radius:100%;
-  background-color:#33353d
+.top .media-icons a:nth-child(2):hover{
+  color: #E1306C;
+  background: #fff;
 }
-.copyright-text
-{
-  margin:0
+.top .media-icons a:nth-child(3){
+  background: #1DA1F2;
 }
-@media (max-width:991px)
-{
-  .site-footer [class^=col-]
-  {
-    margin-bottom:30px
+.top .media-icons a:nth-child(3):hover{
+  color: #1DA1F2;
+  background: #fff;
+}
+.top .media-icons a:nth-child(4){
+  background: #0077B5;
+}
+.top .media-icons a:nth-child(4):hover{
+  color: #0077B5;
+  background: #fff;
+}
+.top .media-icons a:nth-child(5){
+  background: #FF0000;
+}
+.top .media-icons a:nth-child(5):hover{
+  color: #FF0000;
+  background: #fff;
+}
+footer .content .link-boxes{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+footer .content .link-boxes .box{
+  width: calc(100% / 5 - 10px);
+}
+.content .link-boxes .box .link_name{
+  color: #fff;
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 10px;
+  position: relative;
+}
+.link-boxes .box .link_name::before{
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: -2px;
+  height: 2px;
+  width: 35px;
+  background: #fff;
+}
+.content .link-boxes .box li{
+  margin: 6px 0;
+  list-style: none;
+}
+.content .link-boxes .box li a{
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  text-decoration: none;
+  opacity: 0.8;
+  transition: all 0.4s ease
+}
+.content .link-boxes .box li a:hover{
+  opacity: 1;
+  text-decoration: underline;
+}
+.content .link-boxes .input-box{
+  margin-right: 55px;
+}
+.link-boxes .input-box input{
+  height: 40px;
+  width: calc(100% + 55px);
+  outline: none;
+  border: 2px solid #AFAFB6;
+  background: #140B5C;
+  border-radius: 4px;
+  padding: 0 15px;
+  font-size: 15px;
+  color: #fff;
+  margin-top: 5px;
+}
+.link-boxes .input-box input::placeholder{
+  color: #AFAFB6;
+  font-size: 16px;
+}
+.link-boxes .input-box input[type="button"]{
+  background: #fff;
+  color: #140B5C;
+  border: none;
+  font-size: 18px;
+  font-weight: 500;
+  margin: 4px 0;
+  opacity: 0.8;
+  cursor: pointer;
+  transition: all 0.4s ease;
+}
+.input-box input[type="button"]:hover{
+  opacity: 1;
+}
+footer .bottom-details{
+  width: 100%;
+background: #2c303a;
+}
+footer .bottom-details .bottom_text{
+  max-width: 1250px;
+  margin: auto;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+}
+.bottom-details .bottom_text span,
+.bottom-details .bottom_text a{
+  font-size: 14px;
+  font-weight: 300;
+  color: #fff;
+  opacity: 0.8;
+  text-decoration: none;
+}
+.bottom-details .bottom_text a:hover{
+  opacity: 1;
+  text-decoration: underline;
+}
+.bottom-details .bottom_text a{
+  margin-right: 10px;
+}
+@media (max-width: 900px) {
+  footer .content .link-boxes{
+    flex-wrap: wrap;
+  }
+  footer .content .link-boxes .input-box{
+    width: 40%;
+    margin-top: 10px;
   }
 }
-@media (max-width:767px)
-{
-  .site-footer
-  {
-    padding-bottom:0
+@media (max-width: 700px){
+  footer{
+    position: relative;
   }
-  .site-footer .copyright-text,.site-footer .social-icons
-  {
-    text-align:center
+  .content .top .logo-details{
+    font-size: 26px;
   }
-}
-.social-icons
-{
-  padding-left:0;
-  margin-bottom:0;
-  list-style:none
-}
-.social-icons li
-{
-  display:inline-block;
-  margin-bottom:4px
-}
-.social-icons li.title
-{
-  margin-right:15px;
-  text-transform:uppercase;
-  color:#96a2b2;
-  font-weight:700;
-  font-size:13px
-}
-.social-icons a{
-  background-color:#eceeef;
-  color:#818a91;
-  font-size:16px;
-  display:inline-block;
-  line-height:44px;
-  width:44px;
-  height:44px;
-  text-align:center;
-  margin-right:8px;
-  border-radius:100%;
-  -webkit-transition:all .2s linear;
-  -o-transition:all .2s linear;
-  transition:all .2s linear
-}
-.social-icons a:active,.social-icons a:focus,.social-icons a:hover
-{
-  color:#fff;
-  background-color:#29aafe
-}
-.social-icons.size-sm a
-{
-  line-height:34px;
-  height:34px;
-  width:34px;
-  font-size:14px
-}
-.social-icons a.facebook:hover
-{
-  background-color:#3b5998
-}
-.social-icons a.instagram:hover
-{
-  background-color:#bc2a8d
-}
-.social-icons a.GITHUB:hover
-{
-  background-color:#4078c0
-}
-.social-icons a.linkedin:hover
-{
-  background-color:#0077b5
-}
-@media (max-width:767px)
-{
-  .social-icons li.title
-  {
-    display:block;
-    margin-right:0;
-    font-weight:600
+  .content .top .media-icons a{
+    height: 35px;
+    width: 35px;
+    font-size: 14px;
+    line-height: 35px;
+  }
+  footer .content .link-boxes .box{
+    width: calc(100% / 3 - 10px);
+  }
+  footer .content .link-boxes .input-box{
+    width: 60%;
+  }
+  .bottom-details .bottom_text span,
+  .bottom-details .bottom_text a{
+    font-size: 12px;
   }
 }
-  
+@media (max-width: 520px){
+  footer::before{
+    top: 145px;
+  }
+  footer .content .top{
+    flex-direction: column;
+  }
+  .content .top .media-icons{
+    margin-top: 16px;
+  }
+  footer .content .link-boxes .box{
+    width: calc(100% / 2 - 10px);
+  }
+  footer .content .link-boxes .input-box{
+    width: 100%;
+  }
+}
+
 
     </style>
 </head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <body>
-
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-            <!---footer Section Start   ----->
-
-<br><br><br><br>
-
- <!-- Site footer -->
- <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">IAM JAGADEESH   <i>An oriented multitasking professional with good coding techniques pursuing bachelor of engineering degree in Computer Science and Engineering from Sathyabama Institute of Science and Technology [SIST] and have a comprehensive knowledge in development languages for instance C++, Python,java and special talent of writing highly user-friendly programs.</p>
-          </div>
-
-     
-
-          <div class="col-xs-6 col-md-3">
-            <h6>MY WEBPAGE</h6>
-            <ul class="footer-links">
-            <p>
-         <a href="https://jagadeeshpj.herokuapp.com/" target="__blank">https://jagadeeshpj.herokuapp.com/</a>.
-            </p>
-              
-            </ul>
-          </div>
+  <footer>
+    <div class="content">
+      <div class="top">
+        <div class="logo-details">
+          <span class="logo_name">Flipr LMS</span>
         </div>
-        <hr>
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved
+        <div class="media-icons">
+          <a href="https://www.facebook.com/jagadeesh.PJ.13/" target="__blank"><i class="fa fa-facebook"></i></a>
 
-           
-</P>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/jagadeesh.PJ.13/" target="__blank"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="instagram" href="https://www.instagram.com/jagadeesh2001/" target="__blank"><i class="fa fa-instagram"></i></a></li>
-              <li><a class="GITHUB" href="https://github.com/jagadeeshpj"><i class="fa fa-github"></i></a></li>
-              <li><a class="linkedin" href="https://www.linkedin.com/in/p-jagadeesh-22923b18b/" target="__blank"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
+          <a href="https://www.instagram.com/jagadeesh2001/" target="__blank"><i class="fa fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/p-jagadeesh-22923b18b/" target="__blank"><i class="fa fa-linkedin"></i></a>
+          <a href="https://github.com/jagadeeshpj" target="__blank"><i class="fa fa-github"></i></a>
         </div>
       </div>
-</footer>
-            <!---footer Section Ends    ----->
+      <div class="link-boxes">
+        <ul class="box">
+          <li class="link_name">Links</li>
+          <li><a href="../index.php">Home</a></li>
+          
+          <li><a href="https://jagadeeshpj.herokuapp.com/" target="__blank">About Us</a></li>
+          <li><a href="#">Get Started</a></li>
 
+        </ul>
+        <ul class="box">
+          <li class="link_name">Services</li>
+          <li><a href="..\..\programmingdemo.php">My Courses</a></li>
+          <li><a href="..\..\video tutorials\java\display_video_courses.php">Video Tutorials</a></li>
+          <li><a href="..\..\online_quize\quizhome.php">Exercise</a></li>
+          
+
+        </ul>
+        <ul class="box">
+          <li class="link_name">My Website</li>
+          <li><a href="https://jagadeeshpj.herokuapp.com/" target="__blank">about me</a></li>
+          
+
+        </ul>
+        <ul class="box">
+          <li class="link_name">Contact</li>
+          <li><a href="#">+91 9573712XXX</a></li>
+          <li><a href="#" >jagadeeshpj2001@gmail.com</a></li>
+          <li><a href="https://jagadeeshpj.herokuapp.com/" target="__blank">Website</a></li>
+
+        </ul>
+
+      </div>
+    </div>
+    <div class="bottom-details">
+      <div class="bottom_text">
+        <span class="copyright_text">Copyright © 2021 <a href="#">Flipr LMS</a></span>
+        <span class="policy_terms">
+          <a href="#">Privacy policy</a>
+
+        </span>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
-
