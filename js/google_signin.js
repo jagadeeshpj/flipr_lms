37672420186f8195eef8cb8b3ae51c4c9e9e2c2a
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-02 19:12:36 +0530

    Update google_signin.js

--- js/google_signin.js
+++ js/google_signin.js
@@ -1,99 +1,31 @@
- /*function onSignIn(googleUser)
- {
-	
-	window.location="index.php";
-} 
-
-*/
-
-
-<script>
-// Render Google Sign-in button
-function renderButton() {
-    gapi.signin2.render('gSignIn', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
-}
-
-// Sign-in success callback
-function onSuccess(googleUser) {
-    // Get the Google profile data (basic)
-    //var profile = googleUser.getBasicProfile();
-    
-    // Retrieve the Google account data
-    gapi.client.load('oauth2', 'v2', function () {
-        var request = gapi.client.oauth2.userinfo.get({
-            'userId': 'me'
-        });
-        request.execute(function (resp) {
-            // Display the user details
-            var profileHTML = '<h3>Welcome '+resp.given_name+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
-            profileHTML += '<img src="'+resp.picture+'"/><p><b>Google ID: </b>'+resp.id+'</p><p><b>Name: </b>'+resp.name+'</p><p><b>Email: </b>'+resp.email+'</p><p><b>Gender: </b>'+resp.gender+'</p><p><b>Locale: </b>'+resp.locale+'</p><p><b>Google Profile:</b> <a target="_blank" href="'+resp.link+'">click to view profile</a></p>';
-            document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
-            
-            document.getElementById("gSignIn").style.display = "none";
-            document.getElementsByClassName("userContent")[0].style.display = "block";
-        });
-    });
-}
-
-// Sign-in failure callback
-function onFailure(error) {
-    alert(error);
-}
-
-// Sign out the user
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        document.getElementsByClassName("userContent")[0].innerHTML = '';
-        document.getElementsByClassName("userContent")[0].style.display = "none";
-        document.getElementById("gSignIn").style.display = "block";
-    });
-    
-    auth2.disconnect();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Sign-in success callback
-function onSuccess(googleUser) {
-    // Get the Google profile data (basic)
-    //var profile = googleUser.getBasicProfile();
-    
-    // Retrieve the Google account data
-    gapi.client.load('oauth2', 'v2', function () {
-        var request = gapi.client.oauth2.userinfo.get({
-            'userId': 'me'
-        });
-        request.execute(function (resp) {
-            // Display the user details
-            var profileHTML = '<h3>Welcome '+resp.given_name+'! <a href="javascript:void(0);" onclick="signOut();">Sign out</a></h3>';
-            profileHTML += '<img src="'+resp.picture+'"/><p><b>Google ID: </b>'+resp.id+'</p><p><b>Name: </b>'+resp.name+'</p><p><b>Email: </b>'+resp.email+'</p><p><b>Gender: </b>'+resp.gender+'</p><p><b>Locale: </b>'+resp.locale+'</p><p><b>Google Profile:</b> <a target="_blank" href="'+resp.link+'">click to view profile</a></p>';
-            document.getElementsByClassName("userContent")[0].innerHTML = profileHTML;
-            
-            document.getElementById("gSignIn").style.display = "none";
-            document.getElementsByClassName("userContent")[0].style.display = "block";
-            
-            // Save user data
-            saveUserData(resp);
-        });
-    });
-}
-</script>
+ function logout(){
+                    var auth2 = gapi.auth2.getAuthInstance();
+                    auth2.signOut();  
+                    jQuery.ajax({
+                                url:'logout.php',
+                                success:function(result){
+                                        window.location.href="index.php";
+                                }
+                        });
+                    
+                }
+                
+                function onLoad(){
+                       gapi.load('auth2',function (){
+                              gapi.auth2.init();
+                       }); 
+                }
+                
+                function gmailLogIn(userInfo){
+                        var userProfile=userInfo.getBasicProfile();
+                        
+                        
+                        jQuery.ajax({
+                                url:'login_check.php',
+                                type:'post',
+                                data:'user_id='+userProfile.getId()+'&name='+userProfile.getName()+'&image='+userProfile.getImageUrl()+'&email='+userProfile.getEmail(),
+                                success:function(result){
+                                        window.location.href="index.php";
+                                }
+                        });
+                }
