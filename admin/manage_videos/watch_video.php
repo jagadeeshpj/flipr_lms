2d453adf4e809a4c01ad2284b29bf964163154ed
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-07 10:43:14 +0530

    Update watch_video.php

--- admin/manage_videos/watch_video.php
+++ admin/manage_videos/watch_video.php
@@ -68,10 +68,11 @@
 
         <div class="col-sm-2 col-md-2 sidebar badge-dark" style="margin:inherit;" id="sidebar" >
          <ul class="list-group text-white sidebar-list">
-         <li class="list-group-item  bg-dark "><a href="../admin_main.php">Welcome Admin</a></li>
+        <li class="list-group-item  bg-dark "><a href="../admin_main.php">Welcome Admin</a></li>
             <li class="list-group-item bg-dark "><a href="../manage_courses/manage_courses.php">Manage Courses</a></li>
             <li class="list-group-item bg-dark"><a href="../manage_quiz/manage_quiz.php">Manage Quize</a></li>
-            <li class="list-group-item bg-dark"><a href="#">Manage Videos</a></li>
+            <li class="list-group-item bg-dark"><a href="../manage_videos/manage_videos.php">Manage Videos</a></li>
+           
            
             <li class="list-group-item bg-dark"><a href="../../logout.php">Logout</a></li>
             <li class="list-group-item bg-dark" style="height: 400px;"></li>
