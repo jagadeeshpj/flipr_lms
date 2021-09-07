66d0c72cd917ac2aa34c11459fb6e9b5e7950c16
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-07 09:49:13 +0530

    new db

--- video tutorials/java/conn.inc.php
+++ video tutorials/java/conn.inc.php
@@ -2,12 +2,13 @@
 	
 	   
 	 /*  $con=mysqli_connect('localhost','root');  */
- $con=mysqli_connect('remotemysql.com','n8gxap7PGT','hIeFLjY7Qe');
+ $con=mysqli_connect('sql6.freesqldatabase.com','sql6435193','F3REjmVvVA');
+
 			
 	   if (!$con) {
 	   	die('connection failed'.mysqli_connect_error());
 	   }
 
 	/* mysqli_select_db($con,'uniquedeveloper'); */
-mysqli_select_db($con,'n8gxap7PGT');
+mysqli_select_db($con,'sql6435193');
  ?>
