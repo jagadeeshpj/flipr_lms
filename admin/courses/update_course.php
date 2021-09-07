3041c0969aecbbe1f5c7650f358c6e75f574400b
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-07 09:27:24 +0530

    new db

--- admin/courses/update_course.php
+++ admin/courses/update_course.php
@@ -14,10 +14,10 @@ include "../includes/header.php";
 
 	 /*$con=mysqli_connect('localhost','root');
 */
-       		 $con=mysqli_connect('remotemysql.com','n8gxap7PGT','hIeFLjY7Qe');
+       		 $con=mysqli_connect('sql6.freesqldatabase.com','sql6435193','F3REjmVvVA');
 
 
-			mysqli_select_db($con,'n8gxap7PGT');
+			mysqli_select_db($con,'sql6435193');
 			/*mysqli_select_db($con,'uniquedeveloper');*/
        		$course_id=$_GET['course_id'];
        		$q="select * from courses where id='$course_id'";
