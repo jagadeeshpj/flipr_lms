3b3d1706dba36bba917a978e0f63305d6b2ee57a
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-09-07 10:39:52 +0530

    Update manage_courses.php

--- admin/manage_courses/manage_courses.php
+++ admin/manage_courses/manage_courses.php
@@ -165,7 +165,7 @@ body {
         <div class="col-sm-2 col-md-2 sidebar badge-dark" style="margin:inherit;" id="sidebar" >
          <ul class="list-group text-white sidebar-list">
             <li class="list-group-item  bg-dark "><a href="../admin_main.php">Welcome Admin</a></li>
-            <li class="list-group-item bg-dark "><a href="#">Manage Courses</a></li>
+            <li class="list-group-item bg-dark "><a href="../manage_courses/manage_courses.php">Manage Courses</a></li>
             <li class="list-group-item bg-dark"><a href="../manage_quiz/manage_quiz.php">Manage Quize</a></li>
             <li class="list-group-item bg-dark"><a href="../manage_videos/manage_videos.php">Manage Videos</a></li>
             
